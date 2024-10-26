1b108ba10664d2966a3f51ba1c7946bed778251e
Guzman Dynastie <guzmanjrpro@gmail.com>
2024-10-26 12:36:42 -0600

    feat: se agregan funcinalidades
    Inicializaciones, Configuraciones, MIddlewares, Handlebars, Rutas y Archivos

--- src/index.js
+++ src/index.js
@@ -1,30 +1,47 @@
+// import './config/database.js';
 import express from 'express';
-import exphbs from 'express-handlebars';
+import { create } from 'express-handlebars';
+import { fileURLToPath } from 'url';
 import morgan from 'morgan'
 import dotenv from 'dotenv';
 import path from 'path';
+import userRoutes from './routes/user.routes.js'
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Inicializations
 dotenv.config();
 const app = express();
-import './config/database';
 
+
+// Settings
 app.set('port', process.env.PORT || 5000);
 
+// Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('.html', exphbs({
+// Configuration of the Handlebars template engine
+const hbs = create({
     defaultLayout: 'main',
-    layoutDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.html'
-}));
-app.set('view engine', '.html');
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    extname: '.hbs' 
+});
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(require('./routes/login'));
+// Routes
+app.use(userRoutes);
 
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Server is listening
 app.listen(app.get('port'), () => {
     console.log(`Servidor escuchando en el puerto ${app.get('port')}`);
 });
